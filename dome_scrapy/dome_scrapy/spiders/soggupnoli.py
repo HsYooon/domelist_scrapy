e65b2ace2f45f8df076d49dd50bc97dd25c1d612
HsYooon <sssu0000@gmail.com>
2022-09-28 00:08:28 +0900

    :zap: 이미지 경로 수정 및 상품 추가

--- dome_scrapy/dome_scrapy/spiders/soggupnoli.py
+++ dome_scrapy/dome_scrapy/spiders/soggupnoli.py
@@ -12,21 +12,36 @@ class Soggupnoli_Spider(scrapy.Spider) :
 
         uri = 'https://www.soggupnoli.com'
 
-        # 베스트 섹션
+        # 신상품 섹션
         for div in response.xpath('//div[@class="goods_list main_wrap_9"]/div')[1].xpath('./div/ul/li'):
             item = DomeScrapyItem()
 
             url = uri + div.xpath('./div/div[@class="item_photo_box"]/a/@href').get()[2:]
-            img = uri + div.xpath('./div/div[@class="item_photo_box"]/a/img/@src').get()
+            img = div.xpath('./div/div[@class="item_photo_box"]/a/img/@src').get()
             title = div.xpath('./div/div[@class="item_info_cont"]/div')[0].xpath('./a/strong/text()').get()
 
             item['name'] = '소꿉노리'
             item['img'] = img
             item['url'] = url
             item['title'] = title
-            item['category'] = '03' # 애완
-            item['info'] = '12' # 베스트
+            item['category'] = '03' # 인테리어/소품
+            item['info'] = '11' # 신상품
             yield item
 
+        # 베스트 섹션 
+        for div in response.xpath('//div[@class="goods_list main_wrap_7"]/div')[1].xpath('./div/ul/li'):
+            item = DomeScrapyItem()
+
+            url = uri + div.xpath('./div/div[@class="item_photo_box"]/a/@href').get()[2:]
+            img = div.xpath('./div/div[@class="item_photo_box"]/a/img/@src').get()
+            title = div.xpath('./div/div[@class="item_info_cont"]/div')[0].xpath('./a/strong/text()').get()
+
+            item['name'] = '소꿉노리'
+            item['img'] = img
+            item['url'] = url
+            item['title'] = title
+            item['category'] = '03' # 인테리어/소품
+            item['info'] = '12' # 베스트
+            yield item
     
         
