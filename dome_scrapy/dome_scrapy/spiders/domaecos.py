9ab9dc0332789eb2c8a3b73d238b162d03a120fc
HsYooon <sssu0000@gmail.com>
2022-10-23 23:42:12 +0900

    :hammer: 파일명 공백 제거, 바뀐 xpath수정

--- dome_scrapy/dome_scrapy/spiders/domaecos.py
+++ dome_scrapy/dome_scrapy/spiders/domaecos.py
@@ -6,19 +6,20 @@ class Domaecos_Spider(scrapy.Spider) :
 
     name = 'domaecos'
     start_urls = [
-       'http://www.domaecos.com/shop/main/index.php' # 메인페이지 > 신상품 섹션 
+        'https://www.domaecos.com/shop/goods/goods_search.php?page=1&&searched=Y&sort=b.goodsno+desc&page_num=100&skey=all',
+        'https://www.domaecos.com/shop/goods/goods_search.php?page=2&&searched=Y&sort=b.goodsno+desc&page_num=100&skey=all'
+         # 최신등록 & 수정된 상품 200
     ]
 
     def parse(self, response):
 
-        for div in response.xpath('//div[@class="prd_list"]')[1].xpath('./table//td[@align="center"]'):
+        for div in response.xpath('//*[@id="form"]/table/tr[6]/td/table//td[@align="center"]'):
             item = DomeScrapyItem()
-
-            #url = div.xpath('./div[@class="thumb"]//a/@href').get()
+            
             uri = 'https://www.domaecos.com/shop'
             img = uri + div.xpath('./div[@class="thumb"]//a//img/@src').get()[2:]
             title = div.xpath('./div[@class="name"]//a//span/@title').get()
-         
+            
             item['name'] = '도매코스'
             item['img'] = img
             item['url'] = uri #로그인 페이지로 이동 -> 메인이동 고정
