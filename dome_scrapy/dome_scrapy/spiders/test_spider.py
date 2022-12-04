8afa5b050a5768893d867ff2f82ce21cff59086f
HsYooon <sssu0000@gmail.com>
2022-12-04 18:35:51 +0900

    테스트 파일 및 기타 파일 변경

--- dome_scrapy/dome_scrapy/spiders/test_spider.py
+++ dome_scrapy/dome_scrapy/spiders/test_spider.py
@@ -4,48 +4,49 @@ from dome_scrapy.items import DomeScrapyItem
 
 class TestSpider(scrapy.Spider) :
     name = 'test'
-    url_new= 'http://www.lecb2b.com/'
-    url_best = 'http://www.domesangin.com/goods/goods_list.php?cateCd=045'
+    url_new= 'https://accatoy.com/product/list.html?cate_no=49'
+    #url_best = 'https://www.lalab2b.com/goods/goods_list.php?cateCd=009&sort=orderCnt+desc%2Cg.regDt+desc&pageNum=40'
     
     def start_requests(self):
-        yield scrapy.Request(self.url_new, self.parse)
-        #yield scrapy.Request(self.url_best, self.parse_url2)
+        yield scrapy.Request(self.url_new, self.parse1)
+        #yield scrapy.Request(self.url_best, self.parse)
 
-    def parse_url1(self, response):
-        page = response.xpath('//*[@id="contents"]/div/div/div[2]/div[5]/div/ul/li')
-        url = response.url
-        for i in range(1, len(page)+1):
-            target_url = f'{url}&page={i}'
-            yield scrapy.Request(target_url, self.parse)
+    # def parse_url1(self, response):
+    #     page = response.xpath('//*[@id="contents"]/div/div/div[2]/div[5]/div/ul/li')
+    #     url = response.url
+    #     for i in range(1, len(page)+1):
+    #         target_url = f'{url}&page={i}'
+    #         yield scrapy.Request(target_url, self.parse)
 
-    def parse_url2(self, response):
-        page = response.xpath('//*[@id="contents"]/div/div/div[2]/div[4]/div/ul/li')
-        url = response.url
+    # def parse_url2(self, response):
+    #     page = response.xpath('//*[@id="contents"]/div/div/div[2]/div[4]/div/ul/li')
+    #     url = response.url
 
-        for i in range(1, len(page)+1):
-            target_url = f'{url}&page={i}'
-            yield scrapy.Request(target_url, self.parse)
+    #     for i in range(1, len(page)+1):
+    #         target_url = f'{url}&page={i}'
+    #         yield scrapy.Request(target_url, self.parse)
 
-    def parse(self, response):
-       uri = "http://www.lecb2b.com"
+    def parse1(self, response):
+       uri = "https://accatoy.com/"
        
-       for div in response.xpath('//*[@id="mk_center"]/table')[1].xpath('./tr')[8].xpath('./td/table')[2].xpath('./tr/td[@class="lims"]'):
+       for div in response.xpath('//ul[@class="prdList grid4"]/li'):
             item = DomeScrapyItem()
-            url = uri + div.xpath('./table/tr[1]/td/a/@href').get()
-            img = uri + div.xpath('./table/tr[1]/td/a/img/@src').get()
-            title = div.xpath('./table/tr[3]/td/a/font/text()').get().strip(' ')
-            print(url)
-            print(img)
+            
+            url = uri + div.xpath('./div[@class="thumbnail"]/a/@href').get()[1:]
+            img = 'https:' + div.xpath('./div[@class="thumbnail"]/a/img/@src').get()
+            title = div.xpath('./div[@class="description"]/strong/a/span[2]/text()').get()
             print(title)
-            # item['name'] = '필로우커머스'
-            # item['img'] = img
-            # item['url'] = url
-            # item['title'] = title
-            # item['category'] = '01' # 종합
-            # item['info'] = '11'
-            # yield item
-       for div in response.xpath('//*[@id="mk_center"]/table[2]/tr[11]/td/table[3]/tr/td[@class="lims"]'):
-           item = DomeScrapyItem()
-           url = uri + div.xpath('./table/tr[1]/td/a/@href').get()
-           img = uri + div.xpath('./table/tr[1]/td/a/img/@src').get()
-           title = div.xpath('./table/tr[3]/td/a/font/text()').get()
+            
+            
+            item['name'] = '아카토이'
+            item['img'] = img
+            item['url'] = url
+            item['title'] = title
+            item['category'] = '10' # 아동/문구
+            item['info'] = '11'
+            yield item
+    #    for div in response.xpath('//*[@id="mk_center"]/table[2]/tr[11]/td/table[3]/tr/td[@class="lims"]'):
+    #        item = DomeScrapyItem()
+    #        url = uri + div.xpath('./table/tr[1]/td/a/@href').get()
+    #        img = uri + div.xpath('./table/tr[1]/td/a/img/@src').get()
+    #        title = div.xpath('./table/tr[3]/td/a/font/text()').get()
--- dome_scrapy/runschedule.py
+++ dome_scrapy/runschedule.py
@@ -9,81 +9,6 @@ from datetime import datetime
 import configparser as parser
 
 def job():
-    process = CrawlerProcess(get_project_settings())
-    spiders = [
-        'plusb2b',
-        'domaecos',
-        '2sunplanet',
-        'airpodsredbean',
-        # 'autotnb2b',
-        # 'b2boutlet',
-        # 'b2bpet',
-        # 'b2bzoo',
-        # 'bowmall',
-        # 'byramee',
-        # 'dasopoom',
-        # 'dayshouse',
-        # 'dimfac',
-        # 'dodomall',
-        # 'dogsclub',
-        # 'domejjim',
-        # 'domesangin',
-        # 'feelwoo',
-        # 'fromvi',
-        # 'funn',
-        # 'funtasticb2b',
-        # 'gagudome',
-        # 'gcol',
-        # 'ggotda',
-        # 'goodbeauty',
-        # 'goodbuying',
-        # 'hometawnggyi',
-        # 'innovill',
-        # 'iwinwinmarket',
-        # 'jeanforce',
-        # 'jeostyle',
-        # 'joomengi',
-        # 'jsweet',
-        # 'jystyle',
-        # 'jywholesale',
-        # 'kayu',
-        # 'leadersdome',
-        # 'lecb2b',
-        # 'livingchu',
-        # 'manipanda',
-        # 'mdpet',
-        # 'mongtang',
-        # 'moonnri',
-        # 'namdaemun',
-        # 'oneplusm',
-        # 'pettory',
-        # 'pinkshop',
-        # 'plala',
-        # 'pqb2b',
-        # 'roomnoffice',
-        # 'samhoglass',
-        # 'schnariever',
-        # 'sdmall',
-        # 'sellerz',
-        # 'seoulbasket',
-        # 'sijangn',
-        # 'simplo',
-        # 'soggupnoli',
-        # 'sosohome',
-        # 'specialoffer',
-        # 'sujinpet',
-        # 'twostory',
-        # 'wehayou',
-        # 'zseller'
-    ]
-    # 스크롤러 
-    for spider in spiders:
-        process.crawl(spider, domain='scrapy.org')
-        sleep(1.5)
-
-    process.start() # the script will block here until the crawling is finished
-
-    print("++++ Crawl Process finished ++++")
 
     # DB 정보 
     properties = parser.ConfigParser()
@@ -97,29 +22,114 @@ def job():
     char = config['char']
 
     today = (datetime.today().strftime("%Y%m%d"))
+
     # DB 연결
     connection = pymysql.connect(host = host, user= user, password = password, db = db ,charset= char ,autocommit=True)
     cursor = connection.cursor(pymysql.cursors.DictCursor)
+    print("dd")
+    # Auto_increament 확인 
+    sql = f"select max(id) max from t_domelist"
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    for data in res:
+        value = data["max"]
+        if value > 10000:
+            sql = f"DELETE FROM t_domelist where 1=1"
+            cursor.execute(sql)
+            result = cursor.execute("ALTER TABLE t_domelist AUTO_INCREMENT = 1")
+            print(result)
+
+    process = CrawlerProcess(get_project_settings())
+    spiders = [
+        'ezmarket',
+        'plusb2b',
+        'domaecos',
+        '2sunplanet',
+        'airpodsredbean',
+        'autotnb2b',
+        'b2boutlet',
+        'b2bpet',
+        'b2bzoo',
+        'bowmall',
+        'byramee',
+        'dasopoom',
+        'dayshouse',
+        'dimfac',
+        'dodomall',
+        'dogsclub',
+        'domejjim',
+        'domesangin',
+        'feelwoo',
+        'fromvi',
+        'funn',
+        'funtasticb2b',
+        'gagudome',
+        'gcol',
+        'ggotda',
+        'goodbeauty',
+        'goodbuying',
+        'hometawnggyi',
+        'innovill',
+        'iwinwinmarket',
+        'jeanforce',
+        'jeostyle',
+        'joomengi',
+        'jsweet',
+        'jystyle',
+        'jywholesale',
+        'kayu',
+        'leadersdome',
+        'lecb2b',
+        'livingchu',
+        'manipanda',
+        'mdpet',
+        'mongtang',
+        'moonnri',
+        'namdaemun',
+        'oneplusm',
+        'pettory',
+        'pinkshop',
+        'plala',
+        'pqb2b',
+        'roomnoffice',
+        'samhoglass',
+        'schnariever',
+        'sdmall',
+        'sellerz',
+        'seoulbasket',
+        'sijangn',
+        'simplo',
+        'soggupnoli',
+        'sosohome',
+        'specialoffer',
+        'sujinpet',
+        'twostory',
+        'wehayou',
+        'zseller'
+    ]
     
+    # 스크롤러 
+    for spider in spiders:
+        process.crawl(spider, domain='scrapy.org')
+        sleep(1.5)
+
+    process.start() # the script will block here until the crawling is finished
+
+    print("++++ Crawl Process finished ++++")
+
     # 데이터 삭제
     sql = f"DELETE FROM t_domelist WHERE DATE_FORMAT(reg_dttm,'%Y%m%d') < '{today}'"
     result = cursor.execute(sql)
     print(f'=> 총 {result}개의 데이터가 삭제되었습니다.')
 
-    # select = f"SELECT name ,count(*) as 'product count' from t_domelist group by name order by name"
-    # cursor.execute(select)
-    # res = cursor.fetchall()
-    # for data in res:
-    #     print(data)
-    
     # DB 연결 해제
     connection.close()
 
 
-if __name__ == '__main__':
 
-    schedule.every().day.at("11:41").do(job) # 11:41분에 동작
-    schedule.every().day.at("12:00").do(exit) # 프로그램 종료 시간
+if __name__ == '__main__':
+    schedule.every().day.at("22:00").do(job) # 11:41분에 동작
+    schedule.every().day.at("22:05").do(exit) # 프로그램 종료 시간
  
     # 스케쥴을 유지
     while True:
