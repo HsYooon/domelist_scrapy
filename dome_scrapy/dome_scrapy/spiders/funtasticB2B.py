b3fecd34719cbcfc45ab9e4f730172b315e10a36
HsYooon <sssu0000@gmail.com>
2023-07-24 23:23:19 +0900

    :hammer: url 변경

--- dome_scrapy/dome_scrapy/spiders/funtasticB2B.py
+++ dome_scrapy/dome_scrapy/spiders/funtasticB2B.py
@@ -3,14 +3,12 @@ from dome_scrapy.items import DomeScrapyItem
 
 class FuntasticB2B_Spider(scrapy.Spider) :
     name = 'funtasticb2b'
-    start_urls = [
-        'https://funtasticb2b.co.kr/goods/catalog?page=1&code=0023',
-        'https://funtasticb2b.co.kr/goods/catalog?page=2&code=0023',
-        'https://funtasticb2b.co.kr/goods/catalog?page=3&code=0023',
-        'https://funtasticb2b.co.kr/goods/catalog?page=4&code=0023'
-    ]
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request('https://funtasticb2b.co.kr/goods/catalog?perpage=160&code=00280002&popup=&iframe=', self.parse1) # 신상품
+        yield scrapy.Request('https://funtasticb2b.co.kr/goods/catalog?perpage=160&display_style=lattice_a&code=00280004&popup=&iframe=', self.parse2) # 베스트
+
+    def parse1(self, response):
         uri = "https://funtasticb2b.co.kr"
     
         for div in response.xpath('//li[@class="goodsDisplayWrap"]'):
@@ -24,11 +22,34 @@ class FuntasticB2B_Spider(scrapy.Spider) :
             # product title
             title = div.css('.goodsDisplayTextWrap').xpath('.//li[2]//a//span//text()').get()
             # item fetch
-            item['name'] = 'funtasticB2B'
+            item['name'] = '펀타스틱'
             item['img'] = img
             item['url'] = url
             item['title'] = title
-            item['category'] = '01'
-            item['info'] = '11'
+            item['category'] = '01' # 종합
+            item['info'] = '11' # 신상품
+            yield item
+
+        
+    def parse2(self, response):
+        uri = "https://funtasticb2b.co.kr"
+    
+        for div in response.xpath('//li[@class="goodsDisplayWrap"]'):
+            item = DomeScrapyItem()
+        
+            img_tag = div.css('.goodsDisplayImageWrap').xpath('.//img/@src').get()
+            img = uri + img_tag
+            # product link
+            tag_a = div.css('.goodsDisplayTextWrap').xpath('.//li[2]//a/@onclick').get()
+            url = uri + '/goods/view?no=' + tag_a.split("'")[1]
+            # product title
+            title = div.css('.goodsDisplayTextWrap').xpath('.//li[2]//a//span//text()').get()
+            # item fetch
+            item['name'] = '펀타스틱'
+            item['img'] = img
+            item['url'] = url
+            item['title'] = title
+            item['category'] = '01' # 종합
+            item['info'] = '12' # 베스트
             yield item
             
