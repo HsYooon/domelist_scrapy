a937f5b53336df2d64fb45b43704ab642b9ebc99
HsYooon <sssu0000@gmail.com>
2023-07-24 23:27:24 +0900

    :zap: 설정 변경 (CONCURRENT_REQUESTS 외1)

--- dome_scrapy/dome_scrapy/settings.py
+++ dome_scrapy/dome_scrapy/settings.py
@@ -20,8 +20,8 @@ USER_AGENT = 'user'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 5
-
+CONCURRENT_REQUESTS = 16
+REACTOR_THREADPOOL_MAXSIZE = 400
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
